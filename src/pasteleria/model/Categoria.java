7f285d95d0c0d6038563b633b36e6a1fcc4692c6
LAB-USR-AQ265-A0706 <LAB-USR-AQ265-A0706@UTP067313.grupoutp.net>
2019-06-13 19:25:16 -0500

    Add categoria class

---
+++ src/pasteleria/model/Categoria.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasteleria.model;
+
+/**
+ *
+ * @author LAB-USR-AQ265-A0706
+ */
+public class Categoria {
+    private int  idCategoria ;
+}
