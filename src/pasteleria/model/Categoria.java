bb2794cc4fac67ee6c14279bbcf025a7cd1f3d06
JoseLuis <observadorxpl@gmail.com>
2019-06-13 19:28:37 -0500

    Agregado atributo descripcion

--- src/pasteleria/model/Categoria.java
+++ src/pasteleria/model/Categoria.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pasteleria.model;
-
-/**
- *
- * @author LAB-USR-AQ265-A0706
- */
 public class Categoria {
     private int  idCategoria ;
+    private String descripcion;
 }
